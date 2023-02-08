b8094463b29008c216960915d9522374e714dd80
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2023-02-07 23:27:04 -0300

    Aula 02: Thymeleaf e Bootstrap

---
+++ src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
@@ -0,0 +1,71 @@
+package br.com.alura.mvc.mudi.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Pedido {
+
+    private String nomeProduto;
+    private BigDecimal valorNegociado;
+    private LocalDate dataEntrega;
+    private String urlProduto;
+    private String urlImagem;
+    private String descricao;
+
+    public Pedido(String nomeProduto, BigDecimal valorNegociado, LocalDate dataEntrega, String urlProduto, String urlImagem, String descricao) {
+        this.nomeProduto = nomeProduto;
+        this.valorNegociado = valorNegociado;
+        this.dataEntrega = dataEntrega;
+        this.urlProduto = urlProduto;
+        this.urlImagem = urlImagem;
+        this.descricao = descricao;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public BigDecimal getValorNegociado() {
+        return valorNegociado;
+    }
+
+    public void setValorNegociado(BigDecimal valorNegociado) {
+        this.valorNegociado = valorNegociado;
+    }
+
+    public LocalDate getDataEntrega() {
+        return dataEntrega;
+    }
+
+    public void setDataEntrega(LocalDate dataEntrega) {
+        this.dataEntrega = dataEntrega;
+    }
+
+    public String getUrlProduto() {
+        return urlProduto;
+    }
+
+    public void setUrlProduto(String urlProduto) {
+        this.urlProduto = urlProduto;
+    }
+
+    public String getUrlImagem() {
+        return urlImagem;
+    }
+
+    public void setUrlImagem(String urlImagem) {
+        this.urlImagem = urlImagem;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
