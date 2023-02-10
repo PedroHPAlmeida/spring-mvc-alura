2c94a92b16b2556954a54923b4f15a59fa97dfc0
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2023-02-10 19:10:41 -0300

    Aula 05: Trabalhando com Bean Validation

--- src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
@@ -5,10 +5,13 @@ import br.com.alura.mvc.mudi.model.Pedido;
 import br.com.alura.mvc.mudi.repository.PedidoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping(path = "/pedido")
@@ -23,7 +26,10 @@ public class PedidoController {
     }
 
     @PostMapping(path = "/novo")
-    public ModelAndView novo(RequisicaoNovoPedido requisicao) {
+    public ModelAndView novo(@Valid RequisicaoNovoPedido requisicao, BindingResult result) {
+        if(result.hasErrors()) {
+            return new ModelAndView("pedido/formulario");
+        }
         pedidoRepository.save(requisicao.toPedido());
         return new ModelAndView("pedido/formulario");
     }
--- src/main/java/br/com/alura/mvc/mudi/dto/RequisicaoNovoPedido.java
+++ src/main/java/br/com/alura/mvc/mudi/dto/RequisicaoNovoPedido.java
@@ -4,12 +4,18 @@ import br.com.alura.mvc.mudi.model.Pedido;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter @Setter
 public class RequisicaoNovoPedido {
 
+    @NotBlank
     private String nomeProduto;
+    @NotBlank
     private String urlProduto;
+    @NotBlank
     private String urlImagem;
+    @NotBlank
     private String descricao;
 
     public Pedido toPedido() {
