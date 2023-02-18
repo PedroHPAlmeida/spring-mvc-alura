8905dd29e0c96656c77681dfcbefeeb8d398924f
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2023-02-17 21:44:40 -0300

    Curso 2 - Aula 4: Construindo uma API REST

--- src/main/java/br/com/alura/mvc/mudi/controller/OfertaController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/OfertaController.java
@@ -1,2 +1,18 @@
-package br.com.alura.mvc.mudi.controller;public class OfertaController {
+package br.com.alura.mvc.mudi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(path = "oferta")
+public class OfertaController {
+
+    @GetMapping
+    public ModelAndView formularioOfertas() {
+        ModelAndView mv = new ModelAndView("oferta/home");
+        return mv;
+    }
+
 }
